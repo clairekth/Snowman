1140412f646650dc0fa869b2255fe13a48c74b11
Victor Dallé <dallevictor@gmail.com>
2024-02-14 12:21:01 +0100

    Ajout des boutons + yeux collés au snowman

--- model.h
+++ model.h
@@ -4,25 +4,26 @@
 #include <string>
 #include "geometry.h"
 
-class Model {
+class Model
+{
 private:
-    std::vector<Vec3f> verts;
-    std::vector<Vec3i> faces;
+	std::vector<Vec3f> verts;
+	std::vector<Vec3i> faces;
+
 public:
-    Model(const char *filename);
+	Model(const char *filename);
 
-    int nverts() const;                          // number of vertices
-    int nfaces() const;                          // number of triangles
+	int nverts() const; // number of vertices
+	int nfaces() const; // number of triangles
 
-    bool ray_triangle_intersect(const int &fi, const Vec3f &orig, const Vec3f &dir, float &tnear);
+	bool ray_triangle_intersect(const int &fi, const Vec3f &orig, const Vec3f &dir, float &tnear);
 
-    const Vec3f &point(int i) const;                   // coordinates of the vertex i
-    Vec3f &point(int i);                   // coordinates of the vertex i
-    int vert(int fi, int li) const;              // index of the vertex for the triangle fi and local index li
-    void get_bbox(Vec3f &min, Vec3f &max); // bounding box for all the vertices, including isolated ones
+	const Vec3f &point(int i) const;	   // coordinates of the vertex i
+	Vec3f &point(int i);				   // coordinates of the vertex i
+	int vert(int fi, int li) const;		   // index of the vertex for the triangle fi and local index li
+	void get_bbox(Vec3f &min, Vec3f &max); // bounding box for all the vertices, including isolated ones
 };
 
-std::ostream& operator<<(std::ostream& out, Model &m);
+std::ostream &operator<<(std::ostream &out, Model &m);
 
 #endif //__MODEL_H__
-
