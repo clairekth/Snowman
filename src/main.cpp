08b8f53b90bd95e3c2cd196242b863be66c39775
Victor Dallé <dallevictor@gmail.com>
2024-02-19 14:26:35 +0100

    ajout fonctions de bruit + couleurs (à changer) + barre de progression

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,58 @@
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
+#define RAY_EPSILON 0.000001f
+#define RAY_MAX_DIST 1000.f
+#define RAY_ITERATIONS 1024
+
+#define WIDTH 640
+#define HEIGHT 480
+
+template <typename T>
+inline T lerp(const T &v0, const T &v1, float t)
+{
+    return v0 + (v1 - v0) * std::max(0.f, std::min(1.f, t));
+}
+
+float hash(const float n)
+{
+    float x = sin(n) * 43758.5453f;
+    return x - floor(x);
+}
+
+float noise(const Vec3f &x)
+{
+    Vec3f p(floor(x.x), floor(x.y), floor(x.z));
+    Vec3f f(x.x - p.x, x.y - p.y, x.z - p.z);
+    f = f * (f * (Vec3f(3.f, 3.f, 3.f) - f * 2.f));
+    float n = p * Vec3f(1.f, 57.f, 113.f);
+    return lerp(lerp(
+                    lerp(hash(n + 0.f), hash(n + 1.f), f.x),
+                    lerp(hash(n + 57.f), hash(n + 58.f), f.x), f.y),
+                lerp(
+                    lerp(hash(n + 113.f), hash(n + 114.f), f.x),
+                    lerp(hash(n + 170.f), hash(n + 171.f), f.x), f.y),
+                f.z);
+}
+
+Vec3f rotate(const Vec3f &v)
+{
+    return Vec3f(Vec3f(0.00, 0.80, 0.60) * v, Vec3f(-0.80, 0.36, -0.48) * v, Vec3f(-0.60, -0.48, 0.64) * v);
+}
+
+float fractal_brownian_motion(const Vec3f &x)
+{
+    Vec3f p = rotate(x);
+    float f = 0;
+    f += 0.5000 * noise(p);
+    p = p * 2.32;
+    f += 0.2500 * noise(p);
+    p = p * 3.03;
+    f += 0.1250 * noise(p);
+    p = p * 2.61;
+    f += 0.0625 * noise(p);
+    return f / 0.9375;
+}
 
 float sdf_sphere(const Vec3f &p, const Vec3f &center, float radius)
 {
@@ -14,6 +66,12 @@ float sdf_sphere_displaced(const Vec3f &p, const Vec3f &center, float radius, fl
 {
     float displacement = (sin(16 * p.x) * sin(16 * p.y) * sin(16 * p.z) + 1.) / 2.;
     return (p - center).norm() - radius + displacement * noise_amplitude;
+}
+
+float sdf_sphere_displaced_noised(const Vec3f &p, const Vec3f &center, float radius, float noise_amplitude)
+{
+    float displacement = -fractal_brownian_motion(p * 3.4) * noise_amplitude;
+    return (p - center).norm() - radius - displacement;
 }
 
 float smooth_min(float a, float b, float k)
@@ -31,7 +89,7 @@ float map(const Vec3f &orig)
     Vec3f sphere2(0, 1., -2);
     float radius2 = 1.5;
 
-    float d = sdf_sphere_displaced(orig, sphere1, radius1, 0.1);
+    float d = sdf_sphere_displaced_noised(orig, sphere1, radius1, .1);
     d = smooth_min(d, sdf_sphere(orig, sphere2, radius2), 0.2);
 
     return d;
@@ -40,12 +98,14 @@ float map(const Vec3f &orig)
 bool sphere_trace(const Vec3f &orig, const Vec3f &dir, Vec3f &pos)
 {
     pos = orig;
-    for (size_t i = 0; i < 128; i++)
+    for (int i = 0; i < RAY_ITERATIONS; i++)
     {
         float d = map(pos);
-        if (d < 0.)
+        if (d < RAY_EPSILON)
             return true;
         pos = pos + dir * std::max(d * 0.1f, .01f);
+        if (d > RAY_MAX_DIST)
+            break;
     }
     return false;
 }
@@ -68,7 +128,17 @@ Vec3f render(const Vec3f &orig, const Vec3f &dir)
     Vec3f light_dir = (Vec3f(10, 10, 10) - hit).normalize(); // light is placed at (10,10,10)
     float light_intensity = std::max(0.4f, light_dir * distance_field_normal(hit));
 
-    return Vec3f(1, 1, 1) * light_intensity;
+    Vec3f color;
+    if (map(hit) == sdf_sphere_displaced_noised(hit, Vec3f(0, 0, -2), 1.5, .1))
+    {
+        color = Vec3f(1, 0, 0); // couleur de la première sphère
+    }
+    else
+    {
+        color = Vec3f(0, 1, 0); // couleur de la deuxième sphère
+    }
+
+    return color * light_intensity;
 }
 
 int main(int argc, char **argv)
@@ -76,16 +146,30 @@ int main(int argc, char **argv)
     int factor = 1;
     if (argc > 1)
         factor = std::stoi(argv[1]);
-    const int width = 640 * factor, height = 480 * factor;
+    const int width = WIDTH * factor, height = HEIGHT * factor;
     const float fov = M_PI / 3.;
+    float progess = 0;
     std::vector<Vec3f> framebuffer(width * height);
     std::chrono::high_resolution_clock::time_point t1 = std::chrono::high_resolution_clock::now();
 #pragma omp parallel for
     for (int j = 0; j < height; j++)
     { // actual rendering loop
         // TODO: Add a progress bar
-        // FIXME: Is printed in random order
-        // std::cout << height - j << " lines remaining\n" << std::flush;
+        int percent = int(ceil(float(progess / (width * height)) * 100.f));
+        std::cout << "\r\033[K"; // Erase the entire current line.
+        std::cout << "progess : [";
+        if (percent % 5 == 0)
+        {
+            for (int i = 0; i < percent / 5; i++)
+            {
+                std::cout << "#";
+            }
+            for (int i = 0; i < 20 - percent / 5; i++)
+            {
+                std::cout << " ";
+            }
+            std::cout << "] " << percent << "%" << std::flush;
+        }
         for (int i = 0; i < width; i++)
         {
             Vec3f origin(0, 0, 3);
@@ -94,6 +178,7 @@ int main(int argc, char **argv)
             float dir_z = -height / (2. * tan(fov / 2.));
             Vec3f dir = Vec3f(dir_x, dir_y, dir_z).normalize();
             framebuffer[i + j * width] = render(origin, dir);
+            progess++;
         }
     }
     std::chrono::high_resolution_clock::time_point t2 = std::chrono::high_resolution_clock::now();
@@ -111,8 +196,7 @@ int main(int argc, char **argv)
     }
     stbi_write_jpg("out.jpg", width, height, 3, pixmap.data(), 100);
     std::chrono::duration<double> time_span = t2 - t1;
-    // TODO: Add output to png with stb_image : stbi_write_png --> need a pixmap array instead of a framebuffer
-    std::cout << "output generated in " << time_span.count() << " seconds." << std::endl;
+    std::cout << "\noutput generated in " << time_span.count() << " seconds." << std::endl;
 
     return 0;
 }
--- src/main.hpp
+++ src/main.hpp
@@ -8,9 +8,44 @@
 #include <chrono>
 #include "geometry.hpp"
 
+template <typename T>
+inline T lerp(const T &v0, const T &v1, float t);
+
+/**
+ * @brief Function to compute the hash of a float
+ *
+ * @param n Float to compute the hash from
+ * @return float Hash of the float
+ */
+float hash(const float n);
+
+/**
+ * @brief Function to compute the noise of a vector
+ *
+ * @param x Vector to compute the noise from
+ * @return float Noise of the vector
+ */
+float noise(const Vec3f &x);
+
+/**
+ * @brief Function to rotate a vector
+ *
+ * @param v Vector to rotate
+ * @return Vec3f Rotated vector
+ */
+Vec3f rotate(const Vec3f &v);
+
+/**
+ * @brief Function to compute the fractal brownian motion of a vector
+ *
+ * @param x Vector to compute the fractal brownian motion from
+ * @return float Fractal brownian motion of the vector
+ */
+float fractal_brownian_motion(const Vec3f &x);
+
 /**
  * @brief Function to compute the signed distance between a point and a sphere
- * 
+ *
  * @param p Point to compute the distance from
  * @param center Center of the sphere
  * @param radius Radius of the sphere
@@ -20,7 +55,7 @@ float sdf_sphere(const Vec3f &p, const Vec3f &center, float radius);
 
 /**
  * @brief Function to compute the signed distance between a point and a sphere with a displacement
- * 
+ *
  * @param p Point to compute the distance from
  * @param center Center of the sphere
  * @param radius Radius of the sphere
@@ -30,8 +65,19 @@ float sdf_sphere(const Vec3f &p, const Vec3f &center, float radius);
 float sdf_sphere_displaced(const Vec3f &p, const Vec3f &center, float radius, float noise_amplitude);
 
 /**
+ * @brief Function to compute the signed distance between a point and a sphere with a displacement and noise
+ *
+ * @param p Point to compute the distance from
+ * @param center Center of the sphere
+ * @param radius Radius of the sphere
+ * @param noise_amplitude Amplitude of the noise
+ * @return float Signed distance between the point and the sphere
+ */
+float sdf_sphere_displaced_noised(const Vec3f &p, const Vec3f &center, float radius, float noise_amplitude);
+
+/**
  * @brief Function to compute the minimum of two values with a smoothing factor
- * 
+ *
  * @param a First value
  * @param b Second value
  * @param k Smoothing factor
@@ -41,7 +87,7 @@ float smooth_min(float a, float b, float k);
 
 /**
  * @brief Function to compute the distance between a point and the scene
- * 
+ *
  * @param orig Origin of the ray
  * @return float Distance between the point and the scene
  */
@@ -49,7 +95,7 @@ float map(const Vec3f &orig);
 
 /**
  * @brief Function to trace a ray and check if it intersects with the scene
- * 
+ *
  * @param orig Origin of the ray
  * @param dir Direction of the ray
  * @param pos Position of the intersection
@@ -60,7 +106,7 @@ bool sphere_trace(const Vec3f &orig, const Vec3f &dir, Vec3f &pos);
 
 /**
  * @brief Function to compute the normal of the distance field at a given position
- * 
+ *
  * @param pos Position to compute the normal from
  * @return Vec3f Normal of the distance field at the given position
  */
@@ -68,7 +114,7 @@ Vec3f distance_field_normal(const Vec3f &pos);
 
 /**
  * @brief Function to render the scene
- * 
+ *
  * @param orig Origin of the ray
  * @param dir Direction of the ray
  * @return Vec3f Color of the pixel
